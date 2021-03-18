ccaf638d0cd684a5ce570eb61cf33ee158ce6668
MUHAMMAD AKMAL BIN MOHD SABRI <akmalsab14@gmail.com>
2021-03-18 18:10:13 +0800

    initial commit

---
+++ Lab1_Exercise7/src/Input/weight.java
@@ -0,0 +1,14 @@
+package Input;
+
+public class weight {
+
+	private double weightKilogram;
+	
+	public void setWeight(double wkg) {
+		this.weightKilogram = wkg;
+	}
+	
+	public double getWeight() {
+		return weightKilogram;
+	}
+}
---
+++ Lab1_Exercise7/src/Output/result.java
@@ -0,0 +1,21 @@
+package Output;
+
+import Input.weight;
+import Process.calculate;
+import java.util.Scanner;
+
+public class result {
+	
+	static weight w = new weight();
+	static double weightInput;
+	static Scanner s = new Scanner(System.in);
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Enter your weight in Kilogram: ");
+		weightInput = s.nextDouble();
+		w.setWeight(weightInput);
+		calculate c = new calculate(w.getWeight());
+		System.out.println("Your weight in Mars planet is: " + c.result() + " Kg");
+	}
+}
